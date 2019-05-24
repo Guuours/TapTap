4bf5ae6125f170392c3ab6c7a9ddf039ed20c461
guuours <guuours@hotmail.com>
2019-05-24 16:53:33 +0800

    impl

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
+var haveWinner = false;
+io.on('connection', socket => {
+    socket.on('tap', msg => {
+        if (!haveWinner) {
+            haveWinner = true;
+            console.log('>>: ' + msg);
+            socket.broadcast.emit("tapped", msg);
+        }
+    });
+    socket.on('reset', msg => {
+        console.log('>>: reset');
+        haveWinner = false;
+    });
+});
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/index.html'));
+});
+
+app.get('/:color', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/' + req.params.color + '.html'));
 });
 
 // start listening
